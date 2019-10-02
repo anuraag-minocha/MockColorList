58222519583ccc3e4f0a339f2c908b129b1575de
Anuraag Minocha <anuraag.minocha@gmail.com>
2019-10-02 19:13:55 +0530

    Added login activity

--- app/src/main/java/com/android/colorlist/LoginActivity.java
+++ app/src/main/java/com/android/colorlist/LoginActivity.java
@@ -1,13 +1,115 @@
 package com.android.colorlist;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    Button btnLogin;
+    ProgressBar topProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_ativity);
+        setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.editText);
+        etPassword = findViewById(R.id.editText2);
+        btnLogin = findViewById(R.id.button);
+        topProgressBar = findViewById(R.id.topProgressBar);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(etEmail.getText().toString(),etPassword.getText().toString());
+
+            }
+        });
     }
+
+    private void login(final String email, final String password) {
+
+        @SuppressLint("StaticFieldLeak") AsyncTask<Void, Void, String> myTask = new AsyncTask<Void, Void, String>() {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                topProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+
+                String response;
+
+                try {
+
+                    String url = InternetOperations.SERVER_URL+"login";
+
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("email",email);
+                    jsonObject.put("password",password);
+
+                    response = InternetOperations.post(url,jsonObject.toString());
+
+                    return response;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+
+                return null;
+            }
+
+            protected void onPostExecute(String s) {
+
+                topProgressBar.setVisibility(View.GONE);
+
+                if (s != null) {
+
+
+                    try {
+
+
+                        JSONObject object = new JSONObject(s);
+
+                        if(object.has("token")){
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+
+                    }
+
+                }
+
+
+            }
+        };
+        myTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
 }
--- app/src/main/java/com/android/colorlist/MainActivity.java
+++ app/src/main/java/com/android/colorlist/MainActivity.java
@@ -1,13 +1,149 @@
 package com.android.colorlist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar topProgressBar;
+    RecyclerView recyclerView;
+    ColorRecyclerAdapter adapter;
+    ArrayList<Color> colorList = new ArrayList<>();
+    int currentPage = 1;
+    private boolean isLoading = false, isLastPage = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeView();
     }
+
+    public void initializeView(){
+
+        topProgressBar = findViewById(R.id.topProgressBar);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        final LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        adapter = new ColorRecyclerAdapter(this, colorList);
+        recyclerView.setAdapter(adapter);
+
+        loadData();
+
+        RecyclerView.OnScrollListener recyclerViewOnScrollListener = new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int visibleItemCount = mLayoutManager.getChildCount();
+                int totalItemCount = mLayoutManager.getItemCount();
+                int firstVisibleItemPosition = mLayoutManager.findFirstVisibleItemPosition();
+
+                if (!isLoading && !isLastPage) {
+                    if ((visibleItemCount + firstVisibleItemPosition + 1) >= totalItemCount) {
+                        loadData();
+                    }
+                }
+            }
+        };
+
+        recyclerView.addOnScrollListener(recyclerViewOnScrollListener);
+
+    }
+
+    private void loadData() {
+
+            @SuppressLint("StaticFieldLeak") AsyncTask<Void, Void, String> myTask = new AsyncTask<Void, Void, String>() {
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+
+                    topProgressBar.setVisibility(View.VISIBLE);
+                    isLoading = true;
+
+                }
+
+                @Override
+                protected String doInBackground(Void... params) {
+
+                    String response;
+
+                    try {
+
+                        String url = InternetOperations.SERVER_URL+"unknown?page="+currentPage;
+
+                        response = InternetOperations.get(url);
+
+                        return response;
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+
+                    }
+
+                    return null;
+                }
+
+                protected void onPostExecute(String s) {
+
+                    topProgressBar.setVisibility(View.GONE);
+                    isLoading = false;
+
+                    if (s != null) {
+
+
+                        try {
+
+
+                            JSONObject object = new JSONObject(s);
+
+                            JSONArray jsonArray = object.optJSONArray("data");
+
+                            if(currentPage==1)
+                                colorList.clear();
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.optJSONObject(i);
+                                colorList.add(new Color(jsonObject.optString("color"),jsonObject.optString("name")));
+                            }
+
+                            adapter.updateList(colorList);
+
+                            currentPage++;
+
+                            if(jsonArray.length()==0)
+                                isLastPage = true;
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+
+                        }
+
+                    }
+
+
+                }
+            };
+            myTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+
 }
